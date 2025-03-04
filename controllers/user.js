11dd2e4eb711957315a3400d3ef3644528ba129b
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-04 21:52:20 +0700

    feat(controllers/user): add new user's controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,20 @@
+const User = require('../models/User')
+
 const getAllUsers = (req, res) => {
-  return res.status(200).json({
-    message: 'You requested to user handler'
-  })
+  User.find({})
+    .then(users => res.status(200).json({users}))
+    .catch(err => next(err))
+}
+
+const createNewUser = (req, res) => {
+  const newUser = new User(req.body) 
+
+  newUser.save()
+    .then(user => res.status(201).json({user}))
+    .catch(err => next(err))
 }
 
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  createNewUser
 }
