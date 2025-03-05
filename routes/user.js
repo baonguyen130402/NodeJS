1b15ab7d3fdae600111697deb4b5fe3ea4e17177
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-05 18:54:13 +0700

    ref(routes/user): add new routes for user

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,10 @@ const router  = express.Router()
 
 const UserController = require('../controllers/user')
 
-router.route('/getAllUsers').get(UserController.getAllUsers)
 router.route('/createNewUser').post(UserController.createNewUser)
+router.route('/deleteUserById').delete(UserController.deleteUserById)
+router.route('/getAllUsers').get(UserController.getAllUsers)
+router.route('/getUserById').get(UserController.getUserById)
+router.route('/updateUserById').put(UserController.updateUserById)
 
 module.exports = router
