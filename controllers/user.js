89b630b0e553f192ef618ff28dcdf3fd7bf99693
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-05 18:56:02 +0700

    feat(controllers/user): add new user's controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,63 @@
 const User = require('../models/User')
 
-const getAllUsers = (req, res) => {
-  User.find({})
-    .then(users => res.status(200).json({users}))
-    .catch(err => next(err))
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({})
+
+    return res.status(200).json({users})
+  } catch(err) {
+    next(err)
+  }
 }
 
-const createNewUser = (req, res) => {
-  const newUser = new User(req.body) 
+const getUserById = async (req, res, next) => {
+  try {
+    const { userId } = req.body
+    const user = await User.findById(userId)
 
-  newUser.save()
-    .then(user => res.status(201).json({user}))
-    .catch(err => next(err))
+    return res.status(200).json({user})
+  } catch(err) {
+    next(err)
+  }
+}
+
+const updateUserById = async (req, res, next) => {
+  try {
+    const { userId, userData } = req.body
+    await User.findByIdAndUpdate(userId, userData)
+
+    return res.status(200).json({statusCode: 200})
+  } catch(err) {
+    next(err)
+  }
+}
+
+const createNewUser = async (req, res, next) => {
+  try {
+    const newUser = new User(req.body) 
+    await newUser.save()
+
+    return res.status(201).json({user: newUser})
+  } catch(err) {
+    next(err)
+  }
+}
+
+const deleteUserById = async (req, res, next) => {
+  try {
+    const { userId } = req.body
+    await User.findByIdAndDelete(userId)
+
+    return res.status(200).json({statusCode: 200})
+  } catch(err) {
+    next(err)
+  }
 }
 
 module.exports = {
   getAllUsers,
-  createNewUser
+  getUserById,
+  createNewUser,
+  updateUserById,
+  deleteUserById
 }
