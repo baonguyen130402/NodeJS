1faa643304cdabf43dc021091110cf47e95163a4
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-18 22:15:22 +0700

    feat(routes): add new user's routes

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,11 @@ const router  = express.Router()
 const UserController = require('../controllers/user')
 
 router.route('/createNewUser').post(UserController.createNewUser)
+router.route('/createNewDeck').post(UserController.createNewDeck)
 router.route('/deleteUserById').delete(UserController.deleteUserById)
 router.route('/getAllUsers').get(UserController.getAllUsers)
 router.route('/getUserById').get(UserController.getUserById)
+router.route('/getUserDecksById').get(UserController.getUserDecksById)
 router.route('/updateUserById').put(UserController.updateUserById)
 
 module.exports = router
