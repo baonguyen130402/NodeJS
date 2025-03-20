d9b01811ea61ed7c218ee58499eae6aaf85c3e2b
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-20 16:03:49 +0700

    chore(routes): change directory user's controllers

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router  = express.Router()
 
-const UserController = require('../controllers/user')
+const UserController = require('../controllers/User')
 
 router.route('/createNewUser').post(UserController.createNewUser)
 router.route('/createNewDeck').post(UserController.createNewDeck)
